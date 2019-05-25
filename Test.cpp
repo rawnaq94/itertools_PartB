f45c68f6657564369dfcf69a90faa744f0a233b8
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-26 02:43:41 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-using namespace std;
+#include <iostream>
 #include "badkan.hpp"
 #include "chain.hpp"
 #include "powerset.hpp"
 #include "product.hpp"
 #include "range.hpp"
 #include "zip.hpp"
+#include <list>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
 using namespace itertools;
+
 
 int main()
 {
@@ -15,7 +22,7 @@ int main()
 	int signal = setjmp(badkan::longjmp_buffer);
 	if (signal == 0)
 	{
-	testcase.setname("Range test");
+	testcase.setname("range test");
 	int arr=0;
 	for (int i: range(0,35))
 	{
@@ -38,71 +45,113 @@ int main()
 	for (int i: range(101,102))
 	{
 	    testcase.CHECK_EQUAL(i, arr);
-	    arr++;
 	}
 	arr=2;
 	for (int i: range(2,2))
 	{
 	    testcase.CHECK_EQUAL(i, arr);
-	    arr++;
 	}
 	char arr1='a';
-	for (int i: range('a','j'))
+	for (char i: range('a','j'))
 	{
 	    testcase.CHECK_EQUAL(i, arr1);
 	    arr1++;
 	}
 	arr1='d';
-	for (int i: range('d','d'))
+	for (char i: range('d','d'))
 	{
 	    testcase.CHECK_EQUAL(i, arr1);
-	    arr1++;
 	}
 	arr1='u';
-	for (int i: range('u','l'))
+	for (char i: range('u','l'))
 	{
 	    testcase.CHECK_EQUAL(i, arr1);
 	    arr1++;
 	}
 	arr1='~';
-	for (int i: range('~','*'))
+	for (char i: range('~','*'))
 	{
 	    testcase.CHECK_EQUAL(i, arr1);
 	    arr1++;
-	}
+	} 
+		//end of range test.
 	
-	testcase.setname("Chain test");
-
+		
+	testcase.setname("chain test");
 		arr=0;
 		int arr_1[]={0,1,2,3,4,5,10,11,12,13,14};
-		for(int i: chain(range(0,6), range(10,15)))
+		for(auto i: chain(range(0,6), range(10,15)))
 		{
 			testcase.CHECK_EQUAL(i, arr_1[arr]);
 			arr++;				
 		}
 		arr=0;
 		int arr_2[]={0,1,2,3,4,5,0,1,2,3,4,5};
-		for(int i: chain(range(0,6), range(0,6)))
+		for(auto i: chain(range(0,6), range(0,6)))
 		{
 			testcase.CHECK_EQUAL(i, arr_2[arr]);
 			arr++;				
 		}
                 arr=0;
-		char arr_3[]={'a','b','c','d','e','f','g'};
-		for(int i: chain(range('a','d'), range('d','h')))
+		char arr_3[]={'a','b','c','e','f','g'};
+		for(auto i: chain(range('a','d'), range('e','h')))
 		{
 			testcase.CHECK_EQUAL(i, arr_3[arr]);
 			arr++;				
 		}
 		arr=0;
-		char arr_4[]={'a','b','c','A','B','C','D'};
-		for(int i: chain(range('a','d'), range('A','E')))
+		char arr_4[]={'a','b','c','d','e','f','g'};
+		for(auto i: chain(range('a','d'), range('d','h')))
 		{
 			testcase.CHECK_EQUAL(i, arr_4[arr]);
 			arr++;				
 		}
+		arr=0;
+		char arr_5[]={'a','b','c','A','B','C','D'};
+		for(auto i: chain(range('a','d'), range('A','E')))
+		{
+			testcase.CHECK_EQUAL(i, arr_5[arr]);
+			arr++;				
+		}
+		arr=0;
+		char arr_6[]={'a','b','c','h','l','l','o'};
+		for(auto i: chain(range('a','d'), string("hello")))
+		{
+			testcase.CHECK_EQUAL(i, arr_6[arr]);
+			arr++;				
+		}
+		//end chain test.
 		
-    grade = testcase.grade();
+	testcase.setname("zip test");
+		arr=0;
+		string arr_11[]={"1,h","2,e","3,l","4,l","5,o"};
+		for(auto it: zip(range(1,6), string("hello")))
+		{
+			testcase.CHECK_OUTPUT(it, arr_11[arr]);
+			arr++;				
+		}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+    grade = testcase.grade();	
 	}
 	else
 	{
